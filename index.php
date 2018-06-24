cff603677fe312a410ea55127b306ca42567ebb8
Shane Mulligan <mullikine@gmail.com>
2018-06-24 14:08:46 +1200

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<html>
+<head>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.js"></script>
+
+
+</head>
+<body>
+
+
+front page
+
+</body
+</html>
